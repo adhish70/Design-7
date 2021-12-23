6521a176ab4b5783846d0be569fc69677157e235
Adhish Shrivastava <adhish70@gmail.com>
2021-12-23 11:35:03 -0500

    Initial Commit

---
+++ Problem-127 LFU Cache.py
@@ -0,0 +1,24 @@
+# 460. LFU Cache
+# https://leetcode.com/problems/lfu-cache/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        pass        
+
+    def get(self, key: int) -> int:
+        pass
+
+    def put(self, key: int, value: int) -> None:
+        pass
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
---
+++ Problem-128 Design Snake Game.py
@@ -0,0 +1,20 @@
+# 353. Design Snake Game
+# https://leetcode.com/problems/design-snake-game/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class SnakeGame:
+
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        pass
+
+    def move(self, direction: str) -> int:
+        pass
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
